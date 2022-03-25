9b1e8365733c8b168429ad179688a562a7d41149
GoncharovPI <pgoncharov@sdvor.com>
2022-03-25 13:52:44 +0500

    Multiframework

--- SelfHostRunner/Program.cs
+++ SelfHostRunner/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SelfHostRunner
 {
@@ -10,8 +6,7 @@ namespace SelfHostRunner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Hello {Environment.OSVersion}");
-            Console.ReadKey();
+            Console.WriteLine($"Hello World! {Environment.OSVersion}");
         }
     }
 }
